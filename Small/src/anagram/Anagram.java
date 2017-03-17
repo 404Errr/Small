331867055d0a7965100e4d7a1765e84aec577bd4
404Errr <auggieam@gmail.com>
2017-03-17 14:07:29 -0400

    Update Anagram.java

--- Small/src/anagram/Anagram.java
+++ Small/src/anagram/Anagram.java
@@ -1,5 +1,31 @@
+//https://github.com/dwyl/english-words
+//http://app.aspell.net/create
+//https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm#Even.27s_speedup
+//http://stackoverflow.com/questions/2000048/stepping-through-all-permutations-one-swap-at-a-time/11916946#11916946
+
 package anagram;
 
 class Anagram {
+	public static List<String> getAnagrams(String string) {
+		List<String> anagrams = getStringCombos(string);
+		
+		stripInvalid(anagrams);
+		return anagrams;
+	}
 
+	
+	public static List<String> getStringCombos(String wordStr) {
+		List<String> combos = new ArrayList<>();
+		char[] string = wordStr.toCharArray();
+		
+		
+		
+		
+		
+		return combos;
+	}
+	
+	public static void stripInvalid(List<String> str) {
+		//TODO	
+	}
 }
